d83ab8e09e89c25bee286393770e93f069c8959f
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-01-11 14:57:57 +0300

    reactQuery

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import "./reset.css"
+import {
+    QueryClient,
+    QueryClientProvider,
+} from 'react-query'
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <QueryClientProvider client={queryClient}>
   <ChakraProvider>
     <App />
   </ChakraProvider>
+  </QueryClientProvider>
   
 );
 
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import Card from '../../components/Card/Card'
 import { Grid, GridItem } from "@chakra-ui/react";
+import { useQuery } from "react-query";
 
 const Products = () => {
+
+    const { isLoading, error, data } = useQuery("repoData", () =>
+      fetch("https://api.github.com/repos/tannerlinsley/react-query").then(
+        (res) => res.json()
+      )
+    );
+
+    if (isLoading) return "Loading...";
+
+    if (error) return "An error has occurred: " + error.message;
+
+    console.log(data)
+
   return (
     <div>
       <Grid templateColumns="repeat(3, 1fr)" gap={4}>
